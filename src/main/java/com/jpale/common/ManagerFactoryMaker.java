06bf1d96f7e0bfe409f6c79d861146eb91e7ff6d
psk <psk3060@naver.com>
2019-05-10 17:47:44 +0900

    EntityManagerFactory 생성 분리(스레드 안전 위해 ReentrantLock 사용)

---
+++ src/main/java/com/jpale/common/ManagerFactoryMaker.java
@@ -0,0 +1,29 @@
+package com.jpale.common;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.persistence.*;
+
+public class ManagerFactoryMaker {
+    private static EntityManagerFactory managerFactory;
+
+    // 싱글톤으로 생성하기 위한 또다른 방법 syncronized가 아닌
+    private static final ReentrantLock lock = new ReentrantLock();
+
+    private ManagerFactoryMaker() {}
+
+    public static EntityManagerFactory getFactoryInstance() {
+
+        if( managerFactory == null ) {
+            lock.lock();
+
+            try {
+                managerFactory = Persistence.createEntityManagerFactory("learningPersistance"); 
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        return managerFactory;
+    }
+}
