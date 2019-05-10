d009b8455f2eb084a41d05cdf30964702b22a6a9
psk <psk3060@naver.com>
2019-05-10 17:52:16 +0900

    JPA 사용 List 조회

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ JPA 학습 테스트용
 >> EntityManagerFactory 는 하나만 설정되어야 한다(비용이 큼).
 >>> EntityManagerFactory를 하나만 생성하고, EntityManager를 계속 생성하면 된다(CRUD에 사용되는 객체).
 >>>> createQuery에 JPQL을 작성하여 사용할 경우 대소문자 엄격하게 구분함(entity의 클래스명과 동일해야 함)
+>> JPQL 사용 시 asterisk(Select * From)과 같이 사용 못함(Select m From Member m 과 같이 사용해야 함)
 
 > MySQL 사용
--- src/test/java/com/jpale/MemberJpaTest.java
+++ src/test/java/com/jpale/MemberJpaTest.java
@@ -3,21 +3,57 @@ package com.jpale;
 import javax.persistence.Query;
 
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
 import com.jpale.entity.*;
+import com.jpale.common.*;
 
 import javax.persistence.*;
+
+import java.util.*;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class MemberJpaTest {
     private static EntityManagerFactory emf;
 
+    private Logger logger = LoggerFactory.getLogger(MemberJpaTest.class);
+
     @BeforeClass
     public static void setup() {
-        emf = Persistence.createEntityManagerFactory("learningPersistance");
+        emf = ManagerFactoryMaker.getFactoryInstance();
+    }
+
+    @Before
+    public void init() {
+        deleteAll();
+    }
+
+    private void deleteAll() {
+        EntityManager em = emf.createEntityManager();
+
+        String sql = "Delete FROM Member";
+        
+        Query query = em.createQuery(sql);
+
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+
+            query.executeUpdate();
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
     }
 
     @Test
@@ -62,13 +98,57 @@ public class MemberJpaTest {
             long count = (long)q.getSingleResult();
 
             assertThat(count, is(3L));
-
-            tx.commit();
-        } catch(Exception e) {
-            tx.rollback();
         } finally {
+            tx.rollback();
             em.close();
         }
+    }
+
+    @Test
+    public void listTest() {
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        Member member1 = new Member("1", "테스트1", 1, "패스워드1");
+        Member member2 = new Member("2", "테스트2", 2, "패스워드2");
+        Member member3 = new Member("3", "테스트3", 3, "패스워드3");
+        
+        try {
+            tx.begin();
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            em.flush();
+
+            String sql = "SELECT m FROM Member m Order By m.id";
+            
+            Query q = em.createQuery(sql);
+            
+            List<Member> result = q.getResultList();
+
+            assertThat(result.size(), is(3));
+
+            Member m1 = em.find(Member.class, "1");
+            Member m2 = em.find(Member.class, "2");
+            Member m3 = em.find(Member.class, "3");
+
+            assertMember(member1, m1);
+            assertMember(member2, m2);
+            assertMember(member3, m3);
+
+        } finally {
+            tx.rollback();
+            em.close();
+        }
+    }
+
+    private void assertMember(Member m1, Member m2) {
+        assertThat(m1.getUserId(), is(m2.getUserId()));
+        assertThat(m1.getUserName(), is(m2.getUserName()));
+        assertThat(m1.getPassword(), is(m2.getPassword()));
     }
 
     @AfterClass
